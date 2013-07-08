aa953daff55cf29a4f2375eb69a6e5c4748eed9b
Landon Spear <phyujin@gmail.com>
2013-07-07 20:34:49 -0600

    fully parallel and README update

--- MultiQuery.py
+++ MultiQuery.py
@@ -3,12 +3,16 @@
 import sys
 import paramiko
 import traceback
-from multiprocessing import Pool
-from Queue import Queue
+from multiprocessing import Pool, Lock
+
+# Global stdout write lock to synchronize output
+print_lock = Lock()
 
 def query(target):
 	username, hostname = target.split('@')
 	port = 22
+
+	# Check if non-standard port given
 	if hostname.find(':') >= 0:
 		hostname, portstr = hostname.split(':')
 		port = int(portstr)
@@ -17,10 +21,19 @@ def query(target):
 	    client = paramiko.SSHClient()
 	    client.load_system_host_keys()
 	    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+	    # Connect using system SSH agent for authentication
 	    client.connect(hostname, port, username)
+
+	    # Execute uptime on node
 	    stdin, stdout, stderr = client.exec_command('uptime')
+
+	    # Print results on stdout
+	    print_lock.acquire()
 	    for line in stdout:
 	    	print line.strip('\n')
+	    print_lock.release()
+	    
 	    client.close()
 
 	except Exception, e:
@@ -30,24 +43,21 @@ def query(target):
 	        client.close()
 	    except:
 	        pass
-	    sys.exit(1)
 
 def main():
 	# Read destination nodes to be queried
 	with open(sys.argv[1], 'r') as f:
-		node_list = [ line.strip() for line in f.readlines() ]
+		node_list = [ line.strip() for line in f ]
 
 	# Create worker pool with process count equal to cpu_count (default)
 	pool = Pool()
 
 	# Query each node
 	pool.map(query, node_list)
+
+	# Wrap up
 	pool.close()
 	pool.join()
-
-	# Collection of query responses to be consumed by write thread
-	# log_entries = Queue()
-
 
 if __name__ == '__main__':
 	main()
--- README.md
+++ README.md
@@ -5,8 +5,11 @@ Written by Landon Spear (phyujin@gmail.com) as a project for Yola
 
 This is going to be a multi-process script designed to query several hundred servers in parallel for their uptime and print the results on stdout. 
 
-I'm using paramiko (https://github.com/paramiko/paramiko) to handle the SSH connections. The argument MultiQuery takes should be a file containing all desination nodes in the form "username@hostname[:port]". It is assumed that all nodes can be authenticated against the system's known_hosts file and remote authentication can be achieved through the system's ssh_agent (since entering passwords would be far too slow).
-
 To run: ./MultiQuery [target file]
 
-Current status: parallel solution but output still in race condition
+ The argument MultiQuery takes should be a file containing all desination nodes in the form "username@hostname[:port]". I'm using paramiko (https://github.com/paramiko/paramiko) to handle the SSH connections. It is assumed that all nodes can be authenticated against the system's known_hosts file and remote authentication can be achieved by public key authentication through the system's ssh_agent.
+
+ Parallelism is achieved with a worker pool from the multiprocessing library. The pool simply maps the query function on the array of destination addresses.
+
+
+Current status: multi-process parallel solution with output synchronization through global write lock
